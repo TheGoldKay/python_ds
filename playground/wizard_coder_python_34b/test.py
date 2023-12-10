b751e252ac1205eb1b8d9d7a2a48ecdbe404d5ae
TheGoldKay <jhonnatanwfc@live.com>
2023-12-10 19:25:49 -0300

    34b to 3b (34b was too big)

--- playground/wizard_coder_python_34b/test.py
+++ playground/wizard_coder_python_34b/test.py
@@ -1,14 +1,13 @@
-import requests
+import requests, time
 
-API_URL = "https://api-inference.huggingface.co/models/WizardLM/WizardCoder-Python-34B-V1.0"
+API_URL = "https://api-inference.huggingface.co/models/WizardLM/WizardCoder-3B-V1.0"
 headers = {"Authorization": "Bearer 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"}
-
+time.sleep(1)
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 	
 output = query({
-	"inputs": "Can you please let us know more details about your ",
+    "inputs": "Give me an idea of how to cook an egg",
 })
-
 print(output)
